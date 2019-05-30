2050a5b14951695734c818afd90bbee47b32577c
tmdgy6232 <tmdgy6232@naver.com>
2019-05-30 16:44:33 +0900

    java_02

---
+++ src/Hello_01.java
@@ -0,0 +1,25 @@
+
+public class Hello_01 {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("대한민국만세");
+		System.out.println("000000000");
+		System.out.println("OOOOOOOOO");
+		System.out.println("ooooooooo");
+		System.out.println("IIIIIIIII");
+		System.out.println("iiiiiiiii");
+		System.out.println("LLLLLLLLL");
+		System.out.println("lllllllll");
+		System.out.println("111111111");
+
+		boolean bYes = 10 != 20;
+		bYes = 10 >= 20;
+		bYes = 10 <= 20;
+		bYes = 10 
+		
+		+++
+	}
+
+}
